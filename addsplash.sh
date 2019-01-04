21868d262dae7c3f0893588e420515b964a83ce9
ted <t.s@ieee.org>
2019-01-03 16:44:35 -1000

    Update the splash screen update script

--- addsplash.sh
+++ addsplash.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ "$0" == "/bin/bash" ] 
+then 
+  DIR=`dirname $BASH_SOURCE`
+else 
+  DIR=`dirname $0`
+fi
+cd $DIR
 cd classes/artifacts/jsimugate_jar
-./addsplash
+./add-splash
+echo Added splash screen to jar from `pwd` 
+echo To run the jar, use the command:     
+echo java -jar jsimugate.jar
