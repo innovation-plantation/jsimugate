6f167fe4642203014aafe070e126ce61e55df7b3
ted <t.s@ieee.org>
2019-03-13 18:32:35 -1000

    Improved transistor simulation.

--- src/jsimugate/Transistor.java
+++ src/jsimugate/Transistor.java
@@ -71,9 +71,20 @@ public class Transistor extends Discrete {
 
     /**
      * Examine the emitter and base input pins and set the collector output pins accordingly.
+     * Outputting X when the base is unknown is a bit naive. We can do better than that, so...
+     * In the event that the base input is unknown, but there is a good signal on the collector
+     * that is the same polarity as the signal on the emitter, output Z.
      */
     public void operate() {
-        this.c.setOutValue(tt[e.getInValue().ordinal()][b.getInValue().ordinal()]);
+        Signal value = tt[e.getInValue().ordinal()][b.getInValue().ordinal()];
+        this.c.setOutValue(value);
+        if (b.getInValue().bad) {
+            c.recovery=true;
+            if (c.getInValue().hi && e.getInValue().hi) c.setOutValue(Signal._Z);
+            if (c.getInValue().lo && e.getInValue().lo) c.setOutValue(Signal._Z);
+        } else {
+            c.recovery=false;
+        }
     }
 
     public Part reversePolarity() {
